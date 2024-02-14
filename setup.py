4d2ab84e415d8ca80aa3080a18f2bc66cc530859
chrzyki <christoph@functor-argument.net>
2024-02-14 08:32:50 +0100

    Fix dataset ID

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open('metadata.json', encoding='utf-8') as fp:
 
 
 setup(
-    name="lexibank_gaotb",
+    name="lexibank_lionnetyotonahua",
     description=metadata["title"],
     license=metadata.get("license", ""),
     url=metadata.get("url", ""),
-    py_modules=["lexibank_gaotb"],
+    py_modules=["lexibank_lionnetyotonahua"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={"lexibank.dataset": ["gaotb=lexibank_gaotb:Dataset"]},
+    entry_points={"lexibank.dataset": ["lionnetyotonahua=lexibank_lionnetyotonahua:Dataset"]},
     install_requires=["pylexibank>=3"],
     extras_require={"test": ["pytest-cldf"]},
 )
